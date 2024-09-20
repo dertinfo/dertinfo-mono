dd392074d97b1d2ecbd1bbf12903b6b9daca79f7
David Hall <davidsmonkeys@hotmail.com>
2024-09-20 10:32:45 +0100

    Update docker-launch.sh
    Add notes as to environment variable precedence as it matter in this setup.

--- apps/dert-app/infra/docker/docker-launch.sh
+++ apps/dert-app/infra/docker/docker-launch.sh
@@ -3,12 +3,12 @@
 # ######################################################
 # We use a custom start script for the "dertinfo-app" docker container
 # as we need to update the location of the API and the callback url for Auth0.
-# Whnen we run "docker run" or docker-compose up, we can pass environment variables 
+# When we run "docker run" or docker-compose up, we can pass environment variables 
 # to the container. Which we use replace the default values in the containers filesystem
 # from environment variables.
 # ######################################################
 
-# Define the values that we're going to replace in the configuration file. (You can see the actual set values in the Dockerfile)
+# Define the values that we're going to replace in the configuration file. (You can see the actual set default values in the Dockerfile)
 DEFAULT_API_URL=${DEFAULT_API_URL} # http://localhost:44100/api
 DEFAULT_AUTH_CALLBACK_URL=${DEFAULT_AUTH_CALLBACK_URL} # http://localhost:44300/auth/callback
 DEFAULT_ALLOWED_DOMAINS=${DEFAULT_ALLOWED_DOMAINS} # localhost:44100
@@ -17,6 +17,13 @@ DEFAULT_ALLOWED_DOMAINS=${DEFAULT_ALLOWED_DOMAINS} # localhost:44100
 API_URL=${API_URL}
 AUTH_CALLBACK_URL=${AUTH_CALLBACK_URL}
 ALLOWED_DOMAINS=${ALLOWED_DOMAINS}
+# note - configuration presedance in docker compose is (Highest to Lowest): 
+# 1. Command Line -e (will override docker command line - e)
+# 2. Shell Substitute 
+# 3. Environment Set in the docker compose file
+# 4. Use of a --env-file in the docker compose file
+# 5. .env at the root of the repository
+# 7. Set in the container image by ENV X=Y (via the Dockerfile)
 
 # Output the file stucture
 ls -l ./
